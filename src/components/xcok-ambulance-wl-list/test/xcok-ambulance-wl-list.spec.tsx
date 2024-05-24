461681127b7a8edf3c37bd2d4b27c22bce7a8a18
DanielCok17 <cokydano@gmail.com>
2024-05-24 21:23:22 +0200

    unit tests

--- src/components/xcok-ambulance-wl-list/test/xcok-ambulance-wl-list.spec.tsx
+++ src/components/xcok-ambulance-wl-list/test/xcok-ambulance-wl-list.spec.tsx
@@ -7,12 +7,10 @@ describe('xcok-ambulance-wl-list', () => {
       components: [XcokAmbulanceWlList],
       html: `<xcok-ambulance-wl-list></xcok-ambulance-wl-list>`,
     });
-    expect(page.root).toEqualHtml(`
-      <xcok-ambulance-wl-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </xcok-ambulance-wl-list>
-    `);
+    const wlList = page.rootInstance as XcokAmbulanceWlList;
+    const expectedPatients = wlList?.waitingPatients?.length
+
+    const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+    expect(items.length).toEqual(expectedPatients);
   });
 });
